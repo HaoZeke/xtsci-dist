006756885139495be965a808e2c961337c9e2926
Rohit Goswami <rog32@hi.is>
2023-09-14 23:32:58 +0000

    MAINT: Lint and add more dependencies

--- scripts/scipy_test.py
+++ scripts/scipy_test.py
@@ -3,8 +3,8 @@ import numpy as np
 
 np.random.seed(1995)
 A = np.array([0.57955012, 0.9640571, 0.96120518]).reshape(-1, 1)
-euc_dist = distance.pdist(A, metric='euclidean')
-seu_dist = distance.pdist(A, metric='seuclidean')
+euc_dist = distance.pdist(A, metric="euclidean")
+seu_dist = distance.pdist(A, metric="seuclidean")
 print(f"Euclidean distance = {euc_dist} \nScaled Euclidean distance = {seu_dist}")
 var = np.var(A, axis=0, ddof=1)
 print(
