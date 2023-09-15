af2903907df9f518031a855d34ad64da8596e06a
Rohit Goswami <rog32@hi.is>
2023-09-15 00:42:26 +0000

    ENH: Add basic pdist_seuclidean

--- CppCore/include/xtensor_pdist.hpp
+++ CppCore/include/xtensor_pdist.hpp
@@ -29,4 +29,29 @@ template <class E> auto pdist_euclidean(const xt::xexpression<E> &expr) {
   return distances;
 }
 
+template <class E> auto pdist_seuclidean(const xt::xexpression<E> &expr) {
+  // TODO: Support passing in variance like scipy does
+  const auto &mat = expr.derived_cast();
+  auto var = xt::variance(mat, {0}, 1);
+  if (mat.dimension() != 2) {
+    throw std::runtime_error("Input tensor must be 2D");
+  }
+  std::size_t n_rows = mat.shape()[0];
+  std::size_t m_entries = n_rows * (n_rows - 1) / 2;
+  xt::xarray<typename E::value_type> distances =
+      xt::zeros<typename E::value_type>({m_entries});
+
+  size_t k = 0;
+  for (std::size_t i = 0; i < n_rows; ++i) {
+    for (std::size_t j = i + 1; j < n_rows; ++j) {
+      auto diff = xt::row(mat, i) - xt::row(mat, j);
+      auto dist = xt::linalg::norm(diff, 2);
+      distances(k) = dist;
+      k++;
+    }
+  }
+  distances /= xt::sqrt(var);
+  return distances;
+}
+
 } // namespace xts
--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -16,7 +16,9 @@ int main(int argc, char *argv[]) {
   xt::xarray<double> feat = {0.57955012, 0.9640571, 0.96120518};
   feat.reshape({3, 1});
   fmt::print("feat: {}\n", feat);
-  xt::xarray<double> xtpdist = xts::pdist_euclidean(feat);
-  fmt::print("xtpdist: {}\n", xtpdist);
+  xt::xarray<double> xtpd_euc = xts::pdist_euclidean(feat);
+  fmt::print("xtpd_euc: {}\n", xtpd_euc);
+  xt::xarray<double> xtpd_seuc = xts::pdist_seuclidean(feat);
+  fmt::print("xtpd_seuc: {}\n", xtpd_seuc);
   return EXIT_SUCCESS;
 }
