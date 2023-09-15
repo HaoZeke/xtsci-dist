b9315f76c1bb2ecb6338978a7683d5cb7af9dd57
Rohit Goswami <rog32@hi.is>
2023-09-15 01:27:52 +0000

    ENH: Refactor into 1D and ND cases for seuc

--- CppCore/include/xtensor_pdist.hpp
+++ CppCore/include/xtensor_pdist.hpp
@@ -29,13 +29,9 @@ template <class E> auto pdist_euclidean(const xt::xexpression<E> &expr) {
   return distances;
 }
 
-template <class E> auto pdist_seuclidean(const xt::xexpression<E> &expr) {
-  // TODO: Support passing in variance like scipy does
+template <class E> auto pdist_seuclidean_1D(const xt::xexpression<E> &expr) {
   const auto &mat = expr.derived_cast();
-  auto var = xt::variance(mat, {0}, 1);
-  if (mat.dimension() != 2) {
-    throw std::runtime_error("Input tensor must be 2D");
-  }
+  auto var = xt::variance(mat, /*axes=*/{0}, /*ddof=*/1);
   std::size_t n_rows = mat.shape()[0];
   std::size_t m_entries = n_rows * (n_rows - 1) / 2;
   xt::xarray<typename E::value_type> distances =
@@ -50,8 +46,46 @@ template <class E> auto pdist_seuclidean(const xt::xexpression<E> &expr) {
       k++;
     }
   }
-  distances /= xt::sqrt(var);
+  distances /= sqrt(var);
   return distances;
+}
+
+template <class E> auto pdist_seuclidean_ND(const xt::xexpression<E> &expr) {
+  const auto &mat = expr.derived_cast();
+  auto var = xt::variance(mat, /*axes=*/{0}, /*ddof=*/1);
+  auto sqrt_var_array = xt::eval(xt::sqrt(var));
+  sqrt_var_array.reshape({1, sqrt_var_array.shape()[0]});
+  std::size_t n_rows = mat.shape()[0];
+  std::size_t m_entries = n_rows * (n_rows - 1) / 2;
+  xt::xarray<typename E::value_type> distances =
+      xt::zeros<typename E::value_type>({m_entries});
+
+  size_t k = 0;
+  for (std::size_t i = 0; i < n_rows; ++i) {
+    for (std::size_t j = i + 1; j < n_rows; ++j) {
+      auto diff = (xt::row(mat, i) - xt::row(mat, j)) / sqrt_var_array;
+      auto dist = xt::linalg::norm(diff, 2);
+      distances(k) = dist;
+      k++;
+    }
+  }
+  return distances;
+}
+
+template <class E> auto pdist_seuclidean(const xt::xexpression<E> &expr) {
+  // TODO: Support passing in variance like scipy does
+  const auto &mat = expr.derived_cast();
+  if (mat.dimension() != 2) {
+    throw std::runtime_error("Input tensor must be 2D");
+  }
+  auto var = xt::variance(mat, /*axes=*/{0}, /*ddof=*/1);
+  std::size_t n_rows = mat.shape()[0];
+  std::size_t m_entries = n_rows * (n_rows - 1) / 2;
+  if (!var.dimension()) {
+    return pdist_seuclidean_1D(expr);
+  } else {
+    return pdist_seuclidean_ND(expr);
+  }
 }
 
 } // namespace xts
