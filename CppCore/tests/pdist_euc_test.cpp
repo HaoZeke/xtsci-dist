718da11103f9852581bcea133d90e4f4048d70f0
Rohit Goswami <rog32@hi.is>
2023-09-15 02:03:52 +0000

    TST: Add ND tests for eucDist as well

--- CppCore/tests/pdist_euc_test.cpp
+++ CppCore/tests/pdist_euc_test.cpp
@@ -1,5 +1,8 @@
-#include "include/xtensor_pdist.hpp"
+#include "xtensor-io/xnpz.hpp"
 #include "xtensor/xarray.hpp"
+
+#include "include/xtensor_pdist.hpp"
+
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("Testing pdist_euclidean", "[pdist_euclidean]") {
@@ -28,7 +31,21 @@ TEST_CASE("Testing pdist_euclidean", "[pdist_euclidean]") {
     REQUIRE(result(2) == Catch::Approx(1.0));
   }
 
-  SECTION("Test 3: Input is not a 2D tensor") {
+  SECTION("Test 3: A 3x4 matrix") {
+    xt::xarray<double> mat = xt::load_npz<double>("data/inp_3_4.npz", "inp");
+    // TODO: Upstream bug, Layout not supported in immediate reduction.
+    // auto tdat= xt::load_npz("data/inp_3_4.npz");
+    // auto mat = tdat["inp"].cast<double>();
+    xt::xarray<double> expected =
+        xt::load_npz<double>("data/eucDist_3_4.npz", "eucDist");
+    auto result = xts::pdist_euclidean(mat);
+    REQUIRE(result.shape()[0] == 3);
+    for (size_t idx{0}; idx < expected.shape()[0]; ++idx) {
+      REQUIRE(result(idx) == Catch::Approx(expected(idx)));
+    }
+  }
+
+  SECTION("Test 4: Input is not a 2D tensor") {
     xt::xarray<double> mat = {1.0, 2.0, 3.0};
 
     REQUIRE_THROWS_AS(xts::pdist_euclidean(mat), std::runtime_error);
--- CppCore/tests/pdist_seuc_test.cpp
+++ CppCore/tests/pdist_seuc_test.cpp
@@ -1,6 +1,8 @@
-#include "include/xtensor_pdist.hpp"
-#include "xtensor/xarray.hpp"
 #include "xtensor-io/xnpz.hpp"
+#include "xtensor/xarray.hpp"
+
+#include "include/xtensor_pdist.hpp"
+
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("Testing pdist_seuclidean", "[pdist_seuclidean]") {
@@ -29,19 +31,21 @@ TEST_CASE("Testing pdist_seuclidean", "[pdist_seuclidean]") {
     REQUIRE(result(2) == Catch::Approx(1.0));
   }
 
-  SECTION("Test 4: A 3x4 matrix") {
+  SECTION("Test 3: A 3x4 matrix") {
     xt::xarray<double> mat = xt::load_npz<double>("data/inp_3_4.npz", "inp");
-    // Upstream bug, Layout not supported in immediate reduction.
+    // TODO: Upstream bug, Layout not supported in immediate reduction.
     // auto tdat= xt::load_npz("data/inp_3_4.npz");
     // auto mat = tdat["inp"].cast<double>();
+    xt::xarray<double> expected =
+        xt::load_npz<double>("data/seuDist_3_4.npz", "seuDist");
     auto result = xts::pdist_seuclidean(mat);
     REQUIRE(result.shape()[0] == 3);
-    REQUIRE(result(0) == Catch::Approx(3.14375304));
-    REQUIRE(result(1) == Catch::Approx(2.57378885));
-    REQUIRE(result(2) == Catch::Approx(2.73722995));
+    for (size_t idx{0}; idx < expected.shape()[0]; ++idx) {
+      REQUIRE(result(idx) == Catch::Approx(expected(idx)));
+    }
   }
 
-  SECTION("Test 5: Input is not a 2D tensor") {
+  SECTION("Test 4: Input is not a 2D tensor") {
     xt::xarray<double> mat = {1.0, 2.0, 3.0};
 
     REQUIRE_THROWS_AS(xts::pdist_seuclidean(mat), std::runtime_error);
