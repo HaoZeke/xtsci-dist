7396668b7b6f058d6f4b7fca34a14e91d5cc8dde
Rohit Goswami <rog32@hi.is>
2023-09-15 13:17:56 +0000

    MAINT: Add manual tests
    These will need to be moved and refactored into tests

--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 
+#include "distance/cdist/euclidean.hpp"
 #include "distance/pdist/euclidean.hpp"
 #include "distance/pdist/seuclidean.hpp"
 #include "distance/pdist/sqeuclidean.hpp"
@@ -22,18 +23,25 @@ int main(int argc, char *argv[]) {
   static_cast<void>(argv);
   // xt::xarray<double> feat = {0.57955012, 0.9640571, 0.96120518};
   // feat.reshape({3, 1});
-  auto feat = xt::load_npz<double>("inp_3_4.npz", "inp");
-  fmt::print("feat: {}\n", feat);
-  xt::xarray<double> xtpd_euc = xts::distance::pdist::euclidean(feat);
-  fmt::print("xtpd_euc: {}\n", xtpd_euc);
-  xt::xarray<double> xtpd_sqeuc = xts::distance::pdist::sqeuclidean(feat);
-  fmt::print("xtpd_sqeuc: {}\n", xtpd_sqeuc);
-  xt::xarray<double> xtpd_seuc = xts::distance::pdist::seuclidean(feat);
-  fmt::print("xtpd_seuc: {}\n", xtpd_seuc);
+  // auto feat = xt::load_npz<double>("inp_3_4.npz", "inp");
+  // fmt::print("feat: {}\n", feat);
+  // xt::xarray<double> xtpd_euc = xts::distance::pdist::euclidean(feat);
+  // fmt::print("xtpd_euc: {}\n", xtpd_euc);
+  // xt::xarray<double> xtpd_sqeuc = xts::distance::pdist::sqeuclidean(feat);
+  // fmt::print("xtpd_sqeuc: {}\n", xtpd_sqeuc);
+  // xt::xarray<double> xtpd_seuc = xts::distance::pdist::seuclidean(feat);
+  // fmt::print("xtpd_seuc: {}\n", xtpd_seuc);
   // Squareform
-  auto sqform = xts::distance::squareform(xtpd_euc);
-  fmt::print("sqform: {}\n", sqform);
-  auto xtpd_seuc_re = xts::distance::squareform(sqform);
-  fmt::print("xtpd_seuc_re: {}\n", xtpd_seuc_re);
+  // auto sqform = xts::distance::squareform(xtpd_euc);
+  // fmt::print("sqform: {}\n", sqform);
+  // auto xtpd_seuc_re = xts::distance::squareform(sqform);
+  // fmt::print("xtpd_seuc_re: {}\n", xtpd_seuc_re);
+
+  // cdist
+  auto inpa = xt::load_npz<double>("inpa_3_4.npz", "inpa");
+  auto inpb = xt::load_npz<double>("inpb_2_4.npz", "inpb");
+  auto eucCdist = xt::load_npz<double>("eucCdist_3_4_2_4.npz", "eucCdist");
+  xt::xarray<double> eccdist = xts::distance::cdist::euclidean(inpa, inpb);
+  fmt::print("eccdist: {}\n", eccdist);
   return EXIT_SUCCESS;
 }
--- scripts/scipy_test.py
+++ scripts/scipy_test.py
@@ -2,25 +2,38 @@ import numpy as np
 from scipy.spatial import distance
 
 np.random.seed(1995)
-# A = np.array([0.57955012, 0.9640571, 0.96120518]).reshape(-1, 1)
-# A = np.array([1, 2, 3]).reshape(-1, 1)
-inp = np.random.rand(3, 4)
-print(inp)
-eucDist = distance.pdist(inp, metric="euclidean")
-seuDist = distance.pdist(inp, metric="seuclidean")
-sqeuDist = distance.pdist(inp, metric="sqeuclidean")
-print(
-    f"Euclidean distance = {eucDist} \n"
-    f"Scaled Euclidean distance = {seuDist}\n"
-    f"Sq. Euclidean distance = {sqeuDist}"
-)
-print(f"Euc Sq. = {eucDist**2}")
-var = np.var(inp, axis=0, ddof=1)
-print(
-    f"var = {var} and sqrt(var) = {np.sqrt(var)}"
-    # f"\nscaled Euclidean distance is: {eucDist/np.sqrt(var)}"
-)
-np.savez_compressed("inp_3_4.npz", inp=inp)
-np.savez_compressed("eucDist_3_4.npz", eucDist=eucDist)
-np.savez_compressed("seuDist_3_4.npz", seuDist=seuDist)
-np.savez_compressed("sqeuDist_3_4.npz", sqeuDist=sqeuDist)
+# pdist
+# # A = np.array([0.57955012, 0.9640571, 0.96120518]).reshape(-1, 1)
+# # A = np.array([1, 2, 3]).reshape(-1, 1)
+# inp = np.random.rand(3, 4)
+# print(inp)
+# eucDist = distance.pdist(inp, metric="euclidean")
+# seuDist = distance.pdist(inp, metric="seuclidean")
+# sqeuDist = distance.pdist(inp, metric="sqeuclidean")
+# print(
+#     f"Euclidean distance = {eucDist} \n"
+#     f"Scaled Euclidean distance = {seuDist}\n"
+#     f"Sq. Euclidean distance = {sqeuDist}"
+# )
+# print(f"Euc Sq. = {eucDist**2}")
+# var = np.var(inp, axis=0, ddof=1)
+# print(
+#     f"var = {var} and sqrt(var) = {np.sqrt(var)}"
+#     # f"\nscaled Euclidean distance is: {eucDist/np.sqrt(var)}"
+# )
+# np.savez_compressed("inp_3_4.npz", inp=inp)
+# np.savez_compressed("eucDist_3_4.npz", eucDist=eucDist)
+# np.savez_compressed("seuDist_3_4.npz", seuDist=seuDist)
+# np.savez_compressed("sqeuDist_3_4.npz", sqeuDist=sqeuDist)
+
+# cdist
+inpa = np.random.rand(3, 4)
+inpb = np.random.rand(2, 4)
+print(f"inpa: {inpa}")
+print(f"inpb: {inpb}")
+eucCdist = distance.cdist(inpa, inpb, metric="euclidean")
+print(f"Euclidean cdist: {eucCdist}")
+
+np.savez_compressed("inpa_3_4.npz", inpa=inpa)
+np.savez_compressed("inpb_2_4.npz", inpb=inpb)
+np.savez_compressed("eucCdist_3_4_2_4.npz", eucCdist=eucCdist)
